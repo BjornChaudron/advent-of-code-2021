a8c704ec234a478686dc6a42b65b9ca2e63b0169
Björn Chaudron <bjornchaudron@gmail.com>
2021-12-01 22:39:46 +0100

    Solved Task 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+typealias Window = Triple<Int, Int, Int>
+
 fun main() {
     fun part1(input: List<String>): Int {
         val depths = input.map { it.toInt() }
@@ -13,14 +15,34 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        val windows = mutableListOf<Window>()
+
+        for (i in 2..depths.lastIndex) {
+            val window = Window(depths[i - 2], depths[i - 1], depths[i])
+            windows.add(window)
+        }
+
+        val windowSums = windows.map { it.first + it.second + it.third }
+
+        var increases = 0
+
+        for (i in 0 until windowSums.lastIndex) {
+            if (windowSums[i + 1] > windowSums[i]) {
+                increases++
+            }
+        }
+
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
